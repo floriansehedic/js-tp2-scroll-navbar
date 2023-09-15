90055a577f943687ccb0a77b7738f5dc49701e3b
Florian Séhédic <142261246+floriansehedic@users.noreply.github.com>
2023-09-16 01:32:37 +0200

    tp terminé

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+let scrollLvl = 0;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY < scrollLvl) {
+        navbar.style.top = 0;
+    } else {
+        navbar.style.top = "-60px";
+    }
+    scrollLvl =window.scrollY;
+});
