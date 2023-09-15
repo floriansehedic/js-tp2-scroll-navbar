429974aeb3c930122828465ab4960e049d11c7c5
Florian Séhédic <142261246+floriansehedic@users.noreply.github.com>
2023-09-15 21:36:21 +0200

    fichiers de démarrage

---
+++ index.js
@@ -0,0 +1,7 @@
+// Créer un événément au scroll
+
+// Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
+
+// Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
+
+// Connaitre niveau de scroll (window.scrollY)
